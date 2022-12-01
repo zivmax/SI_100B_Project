180c9f316ac6634d32c7f70c0ca6bc41e624cbf7
zivmax <108971427+zivmax@users.noreply.github.com>
2022-12-01 16:26:59 +0800

    test_DTC.py created.

---
+++ codes/digital_tube_control.py
@@ -0,0 +1,198 @@
+import RPi.GPIO as GPIO
+import time
+
+
+DCC = 7
+A = 11
+B = 12
+C = 13
+D = 15
+E = 16
+F = 18
+G = 21
+DP = 22
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(DCC, GPIO.OUT)
+GPIO.setup(A, GPIO.OUT)
+GPIO.setup(B, GPIO.OUT)
+GPIO.setup(C, GPIO.OUT)
+GPIO.setup(D, GPIO.OUT)
+GPIO.setup(E, GPIO.OUT)
+GPIO.setup(F, GPIO.OUT)
+GPIO.setup(G, GPIO.OUT)
+GPIO.setup(DP, GPIO.OUT)
+
+
+def display0():
+
+    GPIO.output(DCC, True)
+    GPIO.output(A, False)
+    GPIO.output(B, False)
+    GPIO.output(C, False)
+    GPIO.output(D, False)
+    GPIO.output(E, False)
+    GPIO.output(F, False)
+    GPIO.output(G, True)
+    GPIO.output(DP, False)
+
+    time.sleep(5)
+
+    GPIO.cleanup()
+
+
+def display1():
+
+    GPIO.output(DCC, True)
+    GPIO.output(A, True)
+    GPIO.output(B, False)
+    GPIO.output(C, False)
+    GPIO.output(D, True)
+    GPIO.output(E, True)
+    GPIO.output(F, True)
+    GPIO.output(G, True)
+    GPIO.output(DP, False)
+
+    time.sleep(5)
+
+    GPIO.cleanup()
+
+
+def Act2():
+
+    GPIO.output(DCC, True)
+    GPIO.output(A, False)
+    GPIO.output(B, False)
+    GPIO.output(C, True)
+    GPIO.output(D, False)
+    GPIO.output(E, False)
+    GPIO.output(F, True)
+    GPIO.output(G, False)
+    GPIO.output(DP, False)
+
+    time.sleep(5)
+
+    GPIO.cleanup()
+
+
+def display3():
+
+    GPIO.output(DCC, True)
+    GPIO.output(A, False)
+    GPIO.output(B, False)
+    GPIO.output(C, False)
+    GPIO.output(D, False)
+    GPIO.output(E, True)
+    GPIO.output(F, True)
+    GPIO.output(G, False)
+    GPIO.output(DP, False)
+
+    time.sleep(5)
+
+    GPIO.cleanup()
+
+
+def display4():
+
+    GPIO.output(DCC, True)
+    GPIO.output(A, True)
+    GPIO.output(B, False)
+    GPIO.output(C, False)
+    GPIO.output(D, True)
+    GPIO.output(E, True)
+    GPIO.output(F, False)
+    GPIO.output(G, False)
+    GPIO.output(DP, False)
+
+    time.sleep(5)
+
+    GPIO.cleanup()
+
+
+def display5():
+
+    GPIO.output(DCC, True)
+    GPIO.output(A, False)
+    GPIO.output(B, True)
+    GPIO.output(C, False)
+    GPIO.output(D, False)
+    GPIO.output(E, True)
+    GPIO.output(F, False)
+    GPIO.output(G, False)
+    GPIO.output(DP, False)
+
+    time.sleep(5)
+
+    GPIO.cleanup()
+
+
+def display6():
+
+    GPIO.output(DCC, True)
+    GPIO.output(A, False)
+    GPIO.output(B, True)
+    GPIO.output(C, False)
+    GPIO.output(D, False)
+    GPIO.output(E, False)
+    GPIO.output(F, False)
+    GPIO.output(G, False)
+    GPIO.output(DP, False)
+
+    time.sleep(5)
+
+    GPIO.cleanup()
+
+
+def display7():
+
+    GPIO.output(DCC, True)
+    GPIO.output(A, False)
+    GPIO.output(B, False)
+    GPIO.output(C, False)
+    GPIO.output(D, True)
+    GPIO.output(E, True)
+    GPIO.output(F, True)
+    GPIO.output(G, True)
+    GPIO.output(DP, False)
+
+    time.sleep(5)
+
+    GPIO.cleanup()
+
+
+def display8():
+
+    GPIO.output(DCC, True)
+    GPIO.output(A, False)
+    GPIO.output(B, False)
+    GPIO.output(C, False)
+    GPIO.output(D, False)
+    GPIO.output(E, False)
+    GPIO.output(F, False)
+    GPIO.output(G, False)
+    GPIO.output(DP, False)
+
+    time.sleep(5)
+
+    GPIO.cleanup()
+
+
+def display9():
+
+    GPIO.output(DCC, True)
+    GPIO.output(A, False)
+    GPIO.output(B, False)
+    GPIO.output(C, False)
+    GPIO.output(D, False)
+    GPIO.output(E, True)
+    GPIO.output(F, False)
+    GPIO.output(G, False)
+    GPIO.output(DP, False)
+
+    time.sleep(5)
+
+    GPIO.cleanupdisplay
+
+
+
---
+++ codes/test_DTC.py
@@ -0,0 +1,39 @@
+import digital_tube_control as dtc
+import time
+
+
+def main():
+
+    dtc.display0()
+    time.sleep(6)
+
+    dtc.display1()
+    time.sleep(6)
+
+    dtc.display2()
+    time.sleep(6)
+
+    dtc.display3()
+    time.sleep(6)
+
+    dtc.display4()
+    time.sleep(6)
+
+    dtc.display5()
+    time.sleep(6)
+
+    dtc.display6()
+    time.sleep(6)
+
+    dtc.display7()
+    time.sleep(6)
+
+    dtc.display8()
+    time.sleep(6)
+
+    dtc.display9()
+    time.sleep(6)
+
+
+if __name__ == "__main__":
+    main()
