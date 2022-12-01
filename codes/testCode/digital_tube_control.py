25ff1b531673313d55746ed9a3f94a4af3f2601a
zivmax <108971427+zivmax@users.noreply.github.com>
2022-12-01 22:58:21 +0800

    .

--- codes/testCode/digital_tube_control.py
+++ codes/testCode/digital_tube_control.py
@@ -2,34 +2,28 @@ import RPi.GPIO as GPIO
 import time
 
 
-# backup the code
-"""
-def setup():
+DCC = 7
+A = 11
+B = 12
+C = 13
+D = 15
+E = 16
+F = 18
+G = 21
+DP = 22
 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(DCC, GPIO.OUT)
+GPIO.setup(A, GPIO.OUT)
+GPIO.setup(B, GPIO.OUT)
+GPIO.setup(C, GPIO.OUT)
+GPIO.setup(D, GPIO.OUT)
+GPIO.setup(E, GPIO.OUT)
+GPIO.setup(F, GPIO.OUT)
+GPIO.setup(G, GPIO.OUT)
+GPIO.setup(DP, GPIO.OUT)   
 
-
-    DCC = 7
-    A = 11
-    B = 12
-    C = 13
-    D = 15
-    E = 16
-    F = 18
-    G = 21
-    DP = 22
-
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(DCC, GPIO.OUT)
-    GPIO.setup(A, GPIO.OUT)
-    GPIO.setup(B, GPIO.OUT)
-    GPIO.setup(C, GPIO.OUT)
-    GPIO.setup(D, GPIO.OUT)
-    GPIO.setup(E, GPIO.OUT)
-    GPIO.setup(F, GPIO.OUT)
-    GPIO.setup(G, GPIO.OUT)
-    GPIO.setup(DP, GPIO.OUT)   
-"""
 
 
 def display0():
--- codes/testCode/test_DTC.py
+++ codes/testCode/test_DTC.py
@@ -4,29 +4,6 @@ import digital_tube_control as dtc
 
 
 def main():
-
-
-    DCC = 7
-    A = 11
-    B = 12
-    C = 13
-    D = 15
-    E = 16
-    F = 18
-    G = 21
-    DP = 22
-
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(DCC, GPIO.OUT)
-    GPIO.setup(A, GPIO.OUT)
-    GPIO.setup(B, GPIO.OUT)
-    GPIO.setup(C, GPIO.OUT)
-    GPIO.setup(D, GPIO.OUT)
-    GPIO.setup(E, GPIO.OUT)
-    GPIO.setup(F, GPIO.OUT)
-    GPIO.setup(G, GPIO.OUT)
-    GPIO.setup(DP, GPIO.OUT)    
     
 
     dtc.display0()
