a24ab7a9760b166beff3de64316958371b41c9cc
zivmax <108971427+zivmax@users.noreply.github.com>
2022-12-07 19:42:37 +0800

    .

--- codes/test_DTC.py
+++ codes/test_DTC.py
@@ -8,52 +8,52 @@ def main():
 
     dtc.setup()
     dtc.display0()
-    time.sleep(0.5)
+    time.sleep(2)
     GPIO.cleanup()
 
     dtc.setup()
     dtc.display1()
-    time.sleep(0.5)
+    time.sleep(2)
     GPIO.cleanup()
 
     dtc.setup()
     dtc.display2()
-    time.sleep(0.5)
+    time.sleep(2)
     GPIO.cleanup()
 
     dtc.setup()
     dtc.display3()
-    time.sleep(0.5)
+    time.sleep(2)
     GPIO.cleanup()
 
     dtc.setup()
     dtc.display4()
-    time.sleep(0.5)
+    time.sleep(2)
     GPIO.cleanup()
 
     dtc.setup()
     dtc.display5()
-    time.sleep(0.5)
+    time.sleep(2)
     GPIO.cleanup()
 
     dtc.setup()
     dtc.display6()
-    time.sleep(0.5)
+    time.sleep(2)
     GPIO.cleanup()
 
     dtc.setup()
     dtc.display7()
-    time.sleep(0.5)
+    time.sleep(2)
     GPIO.cleanup()
 
     dtc.setup()
     dtc.display8()
-    time.sleep(0.5)
+    time.sleep(2)
     GPIO.cleanup()
 
     dtc.setup()
     dtc.display9()
-    time.sleep(0.5)
+    time.sleep(2)
     GPIO.cleanup()
 
 
