d69fbc690f327ba8048758ff85f07004a230c04c
zivmax <108971427+zivmax@users.noreply.github.com>
2022-12-07 12:41:42 +0800

    .

--- codes/testCode/test_DTC.py
+++ codes/testCode/test_DTC.py
@@ -10,50 +10,55 @@ def main():
     dtc.refresh()
 
     dtc.display0()
-    time.sleep(3)
+    time.sleep(1)
 
     GPIO.cleanup()
     dtc.setup()
-
     dtc.display1()
-    time.sleep(3)
-
+    time.sleep(1)
 
     GPIO.cleanup()
     dtc.setup()
-
-
     dtc.display2()
-    time.sleep(3)
-
-
-    GPIO.cleanup()
-    dtc.setup()
-
-
-    dtc.display3()
-    time.sleep(3)
-
+    time.sleep(1)
 
     GPIO.cleanup()
     dtc.setup()
+    dtc.display1()
+    time.sleep(1)
 
+    GPIO.cleanup()
+    dtc.setup()
     dtc.display4()
+    time.sleep(1)
 
     GPIO.cleanup()
     dtc.setup()
-
     dtc.display5()
-
-    dtc.display6()
-
-    dtc.display7()
-
-    dtc.display8()
-
-    dtc.display9()
+    time.sleep(1)
 
     GPIO.cleanup()
+    dtc.setup()
+    dtc.display6()
+    time.sleep(1)
+
+    GPIO.cleanup()
+    dtc.setup()
+    dtc.display7()
+    time.sleep(1)
+
+    GPIO.cleanup()
+    dtc.setup()
+    dtc.display8()
+    time.sleep(1)
+
+    GPIO.cleanup()
+    dtc.setup()
+    dtc.display9()
+    time.sleep(1)
+
+    GPIO.cleanup()
+
 
 if __name__ == "__main__":
     main()
