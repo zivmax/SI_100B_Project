09949c15a9abadc4147943bafbf860aec1062d8e
zivmax <108971427+zivmax@users.noreply.github.com>
2022-12-08 11:39:54 +0800

    .

---
+++ codes/testCode/DTC_test.py
@@ -0,0 +1,60 @@
+import time
+import digital_tube_control as dtc
+
+
+def main():
+
+
+    dtc.setup()
+    dtc.display0()
+    time.sleep(2)
+
+
+
+    dtc.display1()
+    time.sleep(2)
+
+
+
+    dtc.display2()
+    time.sleep(2)
+
+
+
+    dtc.display3()
+    time.sleep(2)
+
+
+
+    dtc.display4()
+    time.sleep(2)
+
+
+
+    dtc.display5()
+    time.sleep(2)
+
+
+
+    dtc.display6()
+    time.sleep(2)
+
+
+
+    dtc.display7()
+    time.sleep(2)
+
+
+
+    dtc.display8()
+    time.sleep(2)
+
+
+
+    dtc.display9()
+    time.sleep(2)
+
+
+
+if __name__ == "__main__":
+    main()
