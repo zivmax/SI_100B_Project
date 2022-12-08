0c75191e2a0182021fdf64382140dc87201ee138
zivmax <108971427+zivmax@users.noreply.github.com>
2022-12-08 18:32:24 +0800

    .

--- codes/testCode/ProjectExercise/button.py
+++ codes/testCode/ProjectExercise/button.py
@@ -19,14 +19,14 @@ def setup():
 
 def detecting_and_shoot(PATH):
     GPIO.add_event_detect(BtnPin, GPIO.FALLING, bouncetime=250)
-    
+
     camera = pc.setup()
     camera.start_preview()
 
     while True:
         if GPIO.event_detected(BtnPin):
             path = pc.shoot(PATH)
-        
+
             return path
 
 
